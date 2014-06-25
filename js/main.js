8de02462db30c6e0a88f448dde06b88a3fe1e45a
Jeremy Huff <jeremythuff@gmail.com>
2014-06-25 00:16:57 -0500

    added event listeners for planets

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 var Game = function() {
     this.canvas = document.createElement('canvas');
     this.ctx = this.canvas.getContext('2d');
-    this.assets = {};
-    this.map = {};
+    this.assets= {
+        images: {},
+        map: {}
+    };
     this.screen.x = 0;
     this.screen.y = 0;
+    this.screen.z = 1;
     return this;
 }
 
@@ -19,16 +22,24 @@ Game.prototype = {
 		game.canvas.width = w;
 	    game.canvas.height = h;
 	      
-        game.loadImages(function() {
-        	game.draw();
+        game.loadAssets(function() {
+        	game.draw(game.screen.x, game.screen.y, game.screen.z);
         });
 
         game.addEventListeners();
     },
+    loadAssets: function(cb) {
+        var game = this;
+        game.loadImages(function() {
+            game.loadMap("test-data", function() { 
+                cb();
+            });
+        });
+    },
     loadImages: function(cb) {
         var game = this;
-        var loadedAssets = 0;
-        var assets = [
+        var loadedImages = 0;
+        var images = [
             {
                 "name": "starfield one",
                 "src": "img/bg.layer.1.jpg",
@@ -92,23 +103,23 @@ Game.prototype = {
 
         ]
         
-        var AssetKeys = Object.keys(assets).length;
+        var AssetKeys = Object.keys(images).length;
 
-        for(var i=0; i<assets.length; i++) {
-            var name = assets[i].name;
-            var src = assets[i].src
-            var group = assets[i].group
-            var settings = assets[i].settings
+        for(var i=0; i<images.length; i++) {
+            var name = images[i].name;
+            var src = images[i].src
+            var group = images[i].group
+            var settings = images[i].settings
 
-            game.assets[name] = new Image;
-            game.assets[name].src = src;
-            game.assets[name].group = group;
-            game.assets[name].settings = settings;
+            game.assets.images[name] = new Image;
+            game.assets.images[name].src = src;
+            game.assets.images[name].group = group;
+            game.assets.images[name].settings = settings;
         }
   		
-        for(var src in game.assets) {
-        	game.assets[src].onload = function() {
-        		if(++loadedAssets >= Object.keys(game.assets).length) cb();		
+        for(var src in game.assets.images) {
+        	game.assets.images[src].onload = function() {
+        		if(++loadedImages >= Object.keys(game.assets.images).length) cb();		
         	}
         }
 
@@ -122,8 +133,7 @@ Game.prototype = {
         request.onload = function() {
           if (request.status >= 200 && request.status < 400){
             var data = JSON.parse(request.responseText);
-            game.map = data;
-            console.log(game.map);
+            game.assets.map = data;
             cb();
           } else {
             console.log("We reached our target server, but it returned an error");
@@ -137,17 +147,17 @@ Game.prototype = {
         request.send();
     },
     setMap: function(map) {
-        this.map = map;
+        this.assets.map = map;
     },
-    draw: function() {
+    draw: function(x,y,z) {
         
         var game = this;
         
         game.canvas.width = game.canvas.width;
         
-        game.drawBG(game.screen.x, game.screen.y);
-        game.drawMap("test-data");
-        game.drawForgrownd();
+        game.drawBG(x,y,z);
+        game.drawMap(x,y,z);
+        game.drawForgrownd(x,y,z);
 
     },
     drawBG: function(startX, startY) {
@@ -155,38 +165,50 @@ Game.prototype = {
 
 
 
-        for(var asset in game.assets) {
-            var name = game.assets[asset].name;
-            var src = game.assets[asset].src;
-            var group = game.assets[asset].group;
-            var settings = game.assets[asset].settings;
+        for(var asset in game.assets.images) {
+            var name = game.assets.images[asset].name;
+            var src = game.assets.images[asset].src;
+            var group = game.assets.images[asset].group;
+            var settings = game.assets.images[asset].settings;
 
             if(group === "background") {
                 game.ctx.globalAlpha  = settings.alpha;    
-                game.ctx.drawImage(game.assets[asset], (startX*settings.layer)+(game.canvas.width*settings.originX), (startY*settings.layer)+(game.canvas.height*settings.originY), game.canvas.width*settings.size, window.innerHeight*settings.size);
+                game.ctx.drawImage(game.assets.images[asset], (startX*settings.layer)+(game.canvas.width*settings.originX), (startY*settings.layer)+(game.canvas.height*settings.originY), game.canvas.width*settings.size, window.innerHeight*settings.size);
                 game.ctx.globalAlpha  = 1; 
             }
         }  
         
     },
-    drawMap: function(map) {
-        
+    drawMap: function(x, y, z) {
+
         var game = this;
+        var w = ((game.canvas.height)*z)-(game.canvas.height*.1);
+        game.assets.map.w = w; 
+        var h = w;
+        game.assets.map.h = h; 
+        var x = (x + ((game.canvas.width/2)-(w/2)))+game.screen.x*4;
+        game.assets.map.x = x;
+        var y = (y + ((game.canvas.height/2)-(h/2)))+game.screen.y*4;
+        game.assets.map.y = y;
+        var oneLightYear = w/2000;
+        var planets = game.assets.map.planets;
 
-        game.loadMap(map, function() {
-            var mapW = game.map.mapWidth;
-            var mapH = game.map.mapHeight;
-            var planets = game.map.planets;
+        for(var i=0; i < planets.length; i++) {
+            var planetX = x + planets[i].XCoordinate*oneLightYear;
+            var planetY = y + planets[i].YCoordinate*oneLightYear;
+            var planetName = planets[i].name;
+            var planetTemp = parseInt(planets[i].temp);
 
-            for(var i=0; i < planets.length; i++) {
-                var x = planets[i].XCoordinate;
-                var y = planets[i].YCoordinate;
-                game.ctx.fillStyle = "#FF0000";
-                game.ctx.fillRect(x,y,3,3);
+            game.ctx.beginPath();
+            game.ctx.arc(planetX, planetY, w/(500), 0, 2 * Math.PI);
+            game.ctx.closePath();
+            game.ctx.fillStyle = "#2f2f2f";
+            game.ctx.fill();
 
-            }
-            
-        });
+            // game.ctx.font = ((z*3)*(planetSize))+'pt Calibri';
+            // game.ctx.fillStyle = 'blue';
+            // game.ctx.fillText(planetName, planetX, planetY-((w/(500/planetSize))+10));
+        }
     },
     drawForgrownd: function() {
     },
@@ -214,21 +236,106 @@ Game.prototype = {
 
 	    }, false);
 
+        this.canvas.addEventListener('mousewheel', function(evt) {
+           game.screen.zoom(game, evt.clientX, evt.clientY, evt.wheelDelta);
+
+        }, false);
+
     },
     screen: {
         move: function($this, direction) {
-            game = $this;
+            var game = $this;
+
             if((typeof(moveScreen)==='undefined')||(moveScreen===false)) {
-               moveScreen = setInterval(function() {           
-                    if(direction === "left") game.screen.x -= 1;
-                    if(direction === "right") game.screen.x += 1;
-                    if(direction === "up") game.screen.y -= 1;
-                    if(direction === "down") game.screen.y += 1;
-                    if(direction != "stop") game.draw(game.screen.x, game.screen.y);
+                moveScreen = setInterval(function() {
+                        if((game.assets.map.w < game.canvas.width)&&(game.assets.map.h < game.canvas.height)) {
+                            if(game.assets.map.x < 10) {
+                                clearInterval(moveScreen);
+                                moveScreen = false;
+                                game.screen.x += 2;
+                            }
+                            if((game.assets.map.x+game.assets.map.w) > game.canvas.width-10) {
+                                clearInterval(moveScreen);
+                                moveScreen = false;
+                                game.screen.x -= 2;
+                            }
+                            if(game.assets.map.y < 10) {
+                                clearInterval(moveScreen);
+                                moveScreen = false;
+                                game.screen.y += 2;
+                            }
+                            if((game.assets.map.y+game.assets.map.h) > game.canvas.height-10) {
+                                clearInterval(moveScreen);
+                                moveScreen = false;
+                                game.screen.y -= 2;
+                            }
+                        } else {
+                            console.log("blah");
+                        }  
+                        if(direction === "left") game.screen.x += 1;
+                        if(direction === "right") game.screen.x -= 1;
+                        if(direction === "up") game.screen.y += 1;
+                        if(direction === "down") game.screen.y -= 1;
+                        if(direction != "stop") game.draw(game.screen.x, game.screen.y, game.screen.z);
+                
                 }, 100); 
             }
+        },
+        zoom: function($this, clientX, clientY, delta) {
+            var game = $this;
+            //get the cursors absolute screen position over the canvas
+            //NOTE this will not work as it is written unless the canvas is in the top left corner of the screen
+            var mouseXOnScreen = clientX;
+            var mouseYOnScreen = clientY;
+
+            //this calculates the position of the mouse over the drawn object on the screen
+            var mouseXOnImg = mouseXOnScreen-game.screen.x;
+            var mouseYOnImg = mouseYOnScreen-game.screen.y;
+
+            //this calculated the cursors offset over the image as a % of the total images size
+            var oldMouseXPosPercentOfImg = mouseXOnImg/(game.canvas.height*game.screen.z);
+            var oldMouseYPosPercentOfImg = mouseYOnImg/(game.canvas.height*game.screen.z);
+
+            /*
+                z is a result of delta squared so that it willl zoom faster the harder you scroll the wheel,
+                one of the deltas is held at an absolut value so that the product will be positive or negative
+                depending on the direction of the wheel's spin, finally z is reduced by a factor of z/n so that
+                zooming will occure more and more quickly as the zoom increases.
+            */
+            game.screen.z += Math.abs(delta)*delta*(game.screen.z/150000);
+            //this places a min/max zoom in/out level
+            game.screen.z<.5 ? game.screen.z=.5: game.screen.z=game.screen.z;
+            game.screen.z>50 ? game.screen.z=50: game.screen.z=game.screen.z;
+
+            // //this recalculates the cursors position as a % of the total images size at the new level of zoom
+            //  var newMouseXPosPercentOfImg = mouseXOnImg/(game.canvas.height*game.screen.z);
+            //  var newMouseYPosPercentOfImg = mouseYOnImg/(game.canvas.height*game.screen.z);
+
+
+
+
+            // //this calculates the difference in the % of the total images size both before and after the zoom
+            // var percentXShift = newMouseXPosPercentOfImg - oldMouseXPosPercentOfImg;
+            // var percentYShift = newMouseYPosPercentOfImg - oldMouseYPosPercentOfImg;
+
+            // // this converts the % into the the relative pixel distance at this level of zoom
+            // var pixelsNowEqualToPercentXShift = (game.screen.x*game.screen.z)*percentXShift;
+            // var pixelsNowEqualToPercentYShift = (game.screen.y*game.screen.z)*percentYShift;
+
+            // //this shifts x and y by the number of pixels represented by the shift in the cursors position relative tot eh image.
+            // game.screen.x += pixelsNowEqualToPercentXShift;
+            // game.screen.y += pixelsNowEqualToPercentYShift;
+
+            game.draw(game.screen.x,game.screen.y,game.screen.z);
+
+            console.log(game.screen.z);
+
         }
     }
+}
+
+var Planet = function() {
+    return this;
 }
 
 var myGame = new Game('myGame');
